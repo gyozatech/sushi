ef5851b87296dfcb5f56ef693f3add5d6c9986cf
Alessandro Argentieri <alexmawashi87@gmail.com>
2023-05-31 09:21:11 +0200

    Update foreach.go

--- functional/foreach.go
+++ functional/foreach.go
@@ -1,12 +1,14 @@
 package functional
 
-// ForEach allows to apply a transformation function over a slice
+// ForEach allows to apply a transformation function over a slice with the following rules:
+// - if one error occurs while processing one item, the whole ForEach function fails;
+// - if processing one item returns a nil element, this will be discarded by the resulting slice
 func ForEach[T any, V any](slice []T, f Function[T, V]) ([]V, error) {
 	newSlice := []V{}
 	for _, t := range slice {
 		if v, err := f(t); err != nil {
 			return nil, err
-		} else {
+		} else if v != nil {
 			newSlice = append(newSlice, *v)
 		}
 	}
